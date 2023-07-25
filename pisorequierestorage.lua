d95dd0acec0256f38a03e7c8289f5dccf375d921
GalaxyCLDev <118214204+GalaxyCLDev@users.noreply.github.com>
2023-07-24 23:11:28 -0400

    Update pisorequierestorage.lua

--- pisorequierestorage.lua
+++ pisorequierestorage.lua
@@ -1,9 +1,11 @@
 local storageRequerido = 1000
 local SKILL_HABILIDAD_MEJORAR = SKILL_SWORD -- Cambiar por la habilidad que desees mejorar
 local SKILL_PUNTOS_A_AGREGAR = 5 -- Cantidad de puntos que se agregarán a la habilidad
+local EFECTOALPISAR = 10 -- Cambiar por el efecto deseado (ver lista de efectos)
+
 
 function onStepIn(cid, item, pos)
-    if item.itemid == 2579 then --- item que es el piso
+    if item.itemid == 2579 then
         if not isPlayer(cid) then
             return true
         end
@@ -11,15 +13,18 @@ function onStepIn(cid, item, pos)
         local hasStorage = getPlayerStorageValue(cid, storageRequerido)
         if hasStorage == 1 then
             doPlayerAddSkillTry(cid, SKILL_HABILIDAD_MEJORAR, SKILL_PUNTOS_A_AGREGAR)
-            doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Has mejorado tu habilidad por entrar a zona vip!")
-       --     doTargetCombatHealth(0, cid, COMBAT_PHYSICALDAMAGE, -15, -30, CONST_ME_NONE) -- esto quita vida
+            doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "You have improved your skill!")
+            doCreatureAddHealth(cid, getCreatureMaxHealth(cid))
+            doPlayerAddMana(cid, getCreatureMaxMana(cid))
             doTransformItem(item.uid, item.itemid - 1)
         else
             doPlayerSendCancel(cid, "You need to complete a mission to pass.")
+            return false -- Evita que el jugador pase si no tiene el storage requerido
         end
+
+        doSendMagicEffect(getCreaturePosition(cid), EFECTOALPISAR)
     else
         if isPlayer(cid) then
-       --     doTargetCombatHealth(0, cid, COMBAT_PHYSICALDAMAGE, -50, -100, CONST_ME_NONE) -- esto quita vida
             doTransformItem(item.uid, item.itemid + 1)
         end
     end
