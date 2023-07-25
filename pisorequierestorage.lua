0d0e255f72c211f95f0e375331f4e65113b46efd
GalaxyCLDev <118214204+GalaxyCLDev@users.noreply.github.com>
2023-07-24 22:58:19 -0400

    Update pisorequierestorage.lua

--- pisorequierestorage.lua
+++ pisorequierestorage.lua
@@ -8,18 +8,18 @@ function onStepIn(cid, item, pos)
             return true
         end
 
-        local hasStorage = getPlayerStorageValue(cid, REQUIRED_STORAGE)
+        local hasStorage = getPlayerStorageValue(cid, storageRequerido)
         if hasStorage == 1 then
-            doPlayerAddSkillTry(cid, SKILL_TO_IMPROVE, SKILL_IMPROVEMENT_AMOUNT)
-            doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "You have improved your skill!")
-            doTargetCombatHealth(0, cid, COMBAT_PHYSICALDAMAGE, -15, -30, CONST_ME_NONE)
+            doPlayerAddSkillTry(cid, SKILL_HABILIDAD_MEJORAR, SKILL_PUNTOS_A_AGREGAR)
+            doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Has mejorado tu habilidad por entrar a zona vip!")
+       --     doTargetCombatHealth(0, cid, COMBAT_PHYSICALDAMAGE, -15, -30, CONST_ME_NONE) -- esto quita vida
             doTransformItem(item.uid, item.itemid - 1)
         else
             doPlayerSendCancel(cid, "You need to complete a mission to pass.")
         end
     else
         if isPlayer(cid) then
-            doTargetCombatHealth(0, cid, COMBAT_PHYSICALDAMAGE, -50, -100, CONST_ME_NONE)
+       --     doTargetCombatHealth(0, cid, COMBAT_PHYSICALDAMAGE, -50, -100, CONST_ME_NONE) -- esto quita vida
             doTransformItem(item.uid, item.itemid + 1)
         end
     end
