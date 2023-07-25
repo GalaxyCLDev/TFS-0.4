699564ea93056df113aef5b033a40b8aaa3c3faa
GalaxyCLDev <118214204+GalaxyCLDev@users.noreply.github.com>
2023-07-24 22:57:00 -0400

    Update pisorequierestorage.lua

--- pisorequierestorage.lua
+++ pisorequierestorage.lua
@@ -1,13 +1,17 @@
 local storageRequerido = 1000
+local SKILL_HABILIDAD_MEJORAR = SKILL_SWORD -- Cambiar por la habilidad que desees mejorar
+local SKILL_PUNTOS_A_AGREGAR = 5 -- Cantidad de puntos que se agregarán a la habilidad
 
 function onStepIn(cid, item, pos)
-    if item.itemid == 2579 then
+    if item.itemid == 2579 then --- item que es el piso
         if not isPlayer(cid) then
             return true
         end
 
-        local hasStorage = getPlayerStorageValue(cid, storageRequerido)
+        local hasStorage = getPlayerStorageValue(cid, REQUIRED_STORAGE)
         if hasStorage == 1 then
+            doPlayerAddSkillTry(cid, SKILL_TO_IMPROVE, SKILL_IMPROVEMENT_AMOUNT)
+            doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "You have improved your skill!")
             doTargetCombatHealth(0, cid, COMBAT_PHYSICALDAMAGE, -15, -30, CONST_ME_NONE)
             doTransformItem(item.uid, item.itemid - 1)
         else
