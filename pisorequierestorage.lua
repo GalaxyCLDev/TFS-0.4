83ddc328caba9c1ee68fb8b6cba086669df6e1d5
GalaxyCLDev <118214204+GalaxyCLDev@users.noreply.github.com>
2023-07-25 00:18:01 -0400

    Update pisorequierestorage.lua

--- pisorequierestorage.lua
+++ pisorequierestorage.lua
@@ -5,28 +5,21 @@ local EFECTOALPISAR = 10 -- Cambiar por el efecto deseado (ver lista de efectos)
 
 
 function onStepIn(cid, item, pos)
-    if item.itemid == 2579 then
-        if not isPlayer(cid) then
-            return true
-        end
+
 
         local hasStorage = getPlayerStorageValue(cid, storageRequerido)
         if hasStorage == 1 then
             doPlayerAddSkillTry(cid, SKILL_HABILIDAD_MEJORAR, SKILL_PUNTOS_A_AGREGAR)
-            doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "You have improved your skill!")
+            doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Ganaste Sword al pasar")
             doCreatureAddHealth(cid, getCreatureMaxHealth(cid))
             doPlayerAddMana(cid, getCreatureMaxMana(cid))
             doTransformItem(item.uid, item.itemid - 1)
         else
-            doPlayerSendCancel(cid, "You need to complete a mission to pass.")
+            doPlayerSendCancel(cid, "Necesitas tener un storage para pasar")
             return false -- Evita que el jugador pase si no tiene el storage requerido
         end
 
         doSendMagicEffect(getCreaturePosition(cid), EFECTOALPISAR)
-    else
-        if isPlayer(cid) then
-            doTransformItem(item.uid, item.itemid + 1)
-        end
-    end
+
     return true
 end
