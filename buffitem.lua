7b7b2ae48640f8662a005e08ef1b59a215c7ca12
GalaxyCLDev <118214204+GalaxyCLDev@users.noreply.github.com>
2023-08-02 02:01:22 -0400

    Update buffitem.lua

--- buffitem.lua
+++ buffitem.lua
@@ -1,35 +1,48 @@
 --- GALAXYDEV
-
 local BUFF_DURATION = 10 * 1000 -- 10 segundos (el valor está en milisegundos)
+local CONDITIONID_HASTE = 1000 -- Puedes cambiar este valor si no está en uso
 
-function onUse(cid, item, fromPosition, itemEx, toPosition)
+function applyBuff(cid)
     -- Si el jugador ya tiene la mejora, no se otorga una nueva.
-    if getPlayerCondition(cid, CONDITION_CUSTOM, CONDITIONID_HASTE) then
+    if getCreatureCondition(cid, CONDITIONID_HASTE, CONDITIONID_HASTE) then
         doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Ya tienes la mejora activa.")
-        return true
+        return
     end
 
     -- Aplica la mejora de vida, maná y habilidades.
-    doPlayerAddHealth(cid, 200) -- Aumenta 200 puntos de vida.
-    doPlayerAddMana(cid, 100)   -- Aumenta 100 puntos de maná.
+    doCreatureAddHealth(cid, 200) -- Aumenta 200 puntos de vida.
+    doCreatureAddMana(cid, 100)   -- Aumenta 100 puntos de maná.
     doPlayerAddSkillTry(cid, SKILL_FIST, 50) -- Aumenta 50 intentos de habilidad de puño.
     doPlayerAddSkillTry(cid, SKILL_SWORD, 50) -- Aumenta 50 intentos de habilidad de espada.
     doPlayerAddSkillTry(cid, SKILL_AXE, 50) -- Aumenta 50 intentos de habilidad de hacha.
 
     -- Crea una condición que removerá los efectos de la mejora después del tiempo de duración.
-    local condition = {type = CONDITION_CUSTOM, subtype = CONDITIONID_HASTE, ticks = BUFF_DURATION}
-    doAddCondition(cid, condition)
+    addEvent(function()
+        if isCreature(cid) then
+            local conditions = getCreatureConditions(cid)
+            for _, condition in ipairs(conditions) do
+                if condition.conditionType == CONDITIONID_HASTE then
+                    doRemoveCondition(cid, CONDITIONID_HASTE)
+                    doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "La mejora ha desaparecido.")
+                    break
+                end
+            end
+        end
+    end, BUFF_DURATION)
 
     doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "¡Has recibido una mejora de vida, maná y habilidades por 10 segundos!")
+end
 
+function onUse(cid, item, fromPosition, itemEx, toPosition)
+    applyBuff(cid)
     return true
 end
 
-function onDeath(cid, corpse, deathList)
-    -- Elimina la condición de mejora cuando el jugador muere.
-    doRemoveCondition(cid, CONDITION_CUSTOM, CONDITIONID_HASTE)
+function onLogin(cid)
+    applyBuff(cid)
     return true
 end
+
 
 
 ---- Abre el archivo data/actions/scripts/custom.lua en un editor de texto o en tu entorno de desarrollo.
