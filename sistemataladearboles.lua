4a623a13ba863ebf32bfe5a6e76d006eda8aaf48
GalaxyCLDev <118214204+GalaxyCLDev@users.noreply.github.com>
2023-08-07 01:14:53 -0400

    Update sistemataladearboles.lua

--- sistemataladearboles.lua
+++ sistemataladearboles.lua
@@ -20,32 +20,40 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
                 -- Recompensas para el jugador (ejemplo: otorgar madera)
                 doPlayerAddItem(cid, 1234, 5) -- ID de la madera y cantidad a otorgar
 
-                -- Actualizar estadísticas y niveles
+                -- Actualizar estadísticas de tala de árboles y otorgar recompensas adicionales
                 local playerId = getPlayerGUID(cid)
                 local stats = db.getResult("SELECT * FROM player_tree_cutting_stats WHERE player_id = " .. playerId)
-                if stats:getID() == -1 then
-                    -- Insertar nuevas estadísticas si no existen
-                    db.executeQuery("INSERT INTO player_tree_cutting_stats (player_id, trees_cut) VALUES (" .. playerId .. ", 1)")
-                else
-                    local treesCut = stats:getDataInt("trees_cut") + 1
+                if stats:getID() ~= -1 then
+                    local treesCut = stats:getDataInt("trees_cut")
                     local currentLevel = stats:getDataInt("tree_cutting_level")
                     local nextLevelTrees = stats:getDataInt("next_level_trees")
-                    
-                    -- Actualizar estadísticas
+
+                    treesCut = treesCut + 1
                     db.executeQuery("UPDATE player_tree_cutting_stats SET trees_cut = " .. treesCut .. " WHERE player_id = " .. playerId)
-                    
+
                     -- Verificar si el jugador sube de nivel
                     if treesCut >= nextLevelTrees and currentLevel < 5 then
                         -- Subir de nivel y reiniciar estadísticas
                         local newLevel = currentLevel + 1
                         local newNextLevelTrees = nextLevelTrees + 150
-                        
+
                         -- Actualizar nivel en la tabla players
                         db.executeQuery("UPDATE players SET tree_cutting_level = " .. newLevel .. " WHERE id = " .. playerId)
-                        
+
                         -- Reiniciar estadísticas en la tabla player_tree_cutting_stats
                         db.executeQuery("UPDATE player_tree_cutting_stats SET trees_cut = 0, tree_cutting_level = " .. newLevel .. ", next_level_trees = " .. newNextLevelTrees .. " WHERE player_id = " .. playerId)
-                        
+
+                        -- Otorgar recompensas adicionales según el nivel
+                        if newLevel == 2 then
+                            doPlayerAddItem(cid, 1234, 10) -- Otorgar 10 unidades de madera adicional
+                        elseif newLevel == 3 then
+                            doPlayerAddItem(cid, 1234, 15) -- Otorgar 15 unidades de madera adicional
+                        elseif newLevel == 4 then
+                            doPlayerAddItem(cid, 1234, 20) -- Otorgar 20 unidades de madera adicional
+                        elseif newLevel == 5 then
+                            doPlayerAddItem(cid, 1234, 25) -- Otorgar 25 unidades de madera adicional
+                        end
+
                         -- Mensaje al jugador
                         doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "¡Has subido al nivel " .. newLevel .. " de tala de árboles!")
                     end
@@ -66,6 +74,40 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 
     return false
 end
+
+
+------------------------------------ agregar en talk actions ----
+
+
+-- En tu archivo commands.lua, agrega el siguiente código:
+
+function onSay(cid, words, param)
+    if param == "" then
+        doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Uso: !treeinfo")
+        return true
+    end
+
+    local player = Player(cid)
+    if player then
+        local stats = db.getResult("SELECT * FROM player_tree_cutting_stats WHERE player_id = " .. player:getGUID())
+        if stats:getID() ~= -1 then
+            local treesCut = stats:getDataInt("trees_cut")
+            local currentLevel = stats:getDataInt("tree_cutting_level")
+            local nextLevelTrees = stats:getDataInt("next_level_trees")
+
+            doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Tala de Árboles - Nivel: " .. currentLevel)
+            doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Árboles talados: " .. treesCut)
+            doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Árboles para próximo nivel: " .. nextLevelTrees)
+        else
+            doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Aún no tienes estadísticas de tala de árboles.")
+        end
+    end
+
+    return true
+end
+
+
+
 
 
 ---- TABLA DE BASE DE DATOS QUE DEBES CREAR ----
