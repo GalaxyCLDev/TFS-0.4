361d1658deb2f701eea7bf6cd57eafdf94f78939
GalaxyCLDev <118214204+GalaxyCLDev@users.noreply.github.com>
2023-08-07 00:49:30 -0400

    Update sistemataladearboles.lua

--- sistemataladearboles.lua
+++ sistemataladearboles.lua
@@ -1,8 +1,14 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-    -- Verificar si el ítem utilizado es un hacha de madera (ID 2001)
-    if item.itemid == 2380 then
-        -- Verificar si el objeto objetivo es un árbol (ID 1001)
-        if itemEx.itemid == 2703 then
+    -- ID del hacha para talar
+    local HACHA_TALAR_ID = 2380
+
+    -- IDs de los árboles a considerar
+    local ARBOLES_TALAR = {2703, 2705, 2710, 2711, 2700}
+
+    -- Verificar si el ítem utilizado es el hacha de talar
+    if item.itemid == HACHA_TALAR_ID then
+        -- Verificar si el objeto objetivo es un árbol para talar
+        if isInArray(ARBOLES_TALAR, itemEx.itemid) then
             -- Iniciar el proceso de tala del árbol
 
             -- Aquí puedes poner el código para el proceso de tala
