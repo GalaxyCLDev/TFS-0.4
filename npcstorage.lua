32630ba4e1cd31a6293703bf8e00050a57d39b9b
GalaxyCLDev <118214204+GalaxyCLDev@users.noreply.github.com>
2023-08-20 13:59:24 -0400

    Update npcstorage.lua

--- npcstorage.lua
+++ npcstorage.lua
@@ -1,4 +1,10 @@
 local requiredStorages = {1000, 1001, 1002, 1003, 1004} -- Lista de storages requeridos
+local shopItems = {
+    {itemid = 2140, price = 10}, -- Ejemplo de item en la tienda: itemid 2140 (Gold Coin) por 10 monedas
+    -- Agrega más elementos a la tienda si es necesario
+}
+
+local currencyItemID = 3636 -- ID del item utilizado como moneda
 
 function onCreatureAppear(cid)
 end
@@ -16,15 +22,20 @@ function onCreatureSay(cid, type, msg)
         end
         
         if #missingStorages == 0 then
-            selfSay("¡Hola, " .. getCreatureName(cid) .. "! ¡Bienvenido!")
-            -- Aquí puedes agregar el código para otorgar el item al jugador
+            selfSay("¡Hola, " .. getCreatureName(cid) .. "! ¡Bienvenido a mi tienda!")
+            selfSay("Te ofrezco los siguientes productos:")
+            
+            for _, item in ipairs(shopItems) do
+                local currencyAmount = getPlayerItemCount(cid, currencyItemID)
+                selfSay("[" .. item.itemid .. "] " .. getItemNameById(item.itemid) .. " - " .. item.price .. " " .. getItemNameById(currencyItemID) .. " (Tienes: " .. currencyAmount .. ")")
+            end
         else
             local missingStorageNames = {}
             for _, storage in ipairs(missingStorages) do
                 table.insert(missingStorageNames, "Storage " .. storage)
             end
             local missingStorageList = table.concat(missingStorageNames, ", ")
-            selfSay("¡Hola, " .. getCreatureName(cid) .. "! Para hablar conmigo, necesitas completar los siguientes storages: " .. missingStorageList)
+            selfSay("¡Hola, " .. getCreatureName(cid) .. "! Para hablar conmigo y acceder a la tienda, necesitas completar los siguientes storages: " .. missingStorageList)
         end
     end
 end
